fd8e10b5dab724841d88fb451d7224d749df89da
bruna-blip <bruna@topconsultoriaambiental.com>
2026-03-08 19:52:26 -0300

    Delete js/supabase-config.js

--- js/supabase-config.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://gksiyscqysrdhyzmwfen.supabase.co";
-const SUPABASE_KEY = "sb_publishable_lucWMhtuCtocA-ud7dnFBQ_ewoGauJL";
-
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
-
