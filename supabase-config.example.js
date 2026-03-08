4c44dc31b90d31eb3c54dbecc0d94051d80240a4
bruna-blip <bruna@topconsultoriaambiental.com>
2026-03-08 12:16:07 -0300

    Add files via upload

---
+++ supabase-config.example.js
@@ -0,0 +1,6 @@
+window.GEO_AMBIENTAL_CONFIG = {
+  supabaseUrl: 'https://SEU-PROJETO.supabase.co',
+  supabaseAnonKey: 'SUA_ANON_KEY_AQUI',
+  loginPage: 'login.html',
+  dashboardPage: 'index.html',
+};
