9768926a0b9c931a534f32fddbbcaa4e8ce2f5c2
bruna-blip <bruna@topconsultoriaambiental.com>
2026-03-08 17:02:22 -0300

    Delete supabase-config.example.js

--- supabase-config.example.js
+++
@@ -1,6 +0,0 @@
-window.GEO_AMBIENTAL_CONFIG = {
-  supabaseUrl: 'https://SEU-PROJETO.supabase.co',
-  supabaseAnonKey: 'SUA_ANON_KEY_AQUI',
-  loginPage: 'login.html',
-  dashboardPage: 'index.html',
-};
